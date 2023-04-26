34dfcb80fe4460efdb495cf85046c9687cb50bf0
Robin <82582947+illumou@users.noreply.github.com>
2023-04-26 22:05:48 +0200

    Changes layout

--- pages/draggable.vue
+++ pages/draggable.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="w-full h-screen bg-[#111827] flex items-center justify-between flex-col space-y-3">
-    <a class="m-4 text-white underline" target="_blank" href="https://github.com/illumou/vue-dragger"> Github Repo </a>
-    <input
-        v-model="newTask"
-        class="w-96 mt-10 space-y-0 rounded"
-        type="text"
-        placeholder="New Task"
-        @keypress.enter="addToDo"
-    >
-    <div class="mt-20 w-full h-full flex justify-center">
-      <div v-for="lane in lanes">
-        <div class="px-3 py-3 m-2 bg-gray-300 rounded-lg w-96 min-h-[8rem] flex flex-col">
-          <div class="flex flex-row w-full justify-between border-b-2" :style="{borderColor: lane.border}">
-            <p class="text-gray-700">
-              {{ lane.name }}
-            </p>
-            <button @click="addTask(lane.id)" class="text-gray-700">Add Task</button>
+  <main class="w-full h-screen bg-[#111827]">
+    <div class="flex flex-col space-y-5">
+      <div class="flex justify-center items-center flex-col justify-between gap-y-2">
+        <a class="mt-4 text-white underline" target="_blank" href="https://github.com/illumou/vue-dragger"> Github Repo </a>
+        <input
+            v-model="newTask"
+            class="w-96 h-10 p-2 font-medium space-y-0 rounded"
+            type="text"
+            placeholder="New Task"
+            @keypress.enter="addToDo"
+        >
+      </div>
+      <div class="w-full h-full flex justify-center">
+        <div v-for="lane in lanes">
+          <div class="px-3 py-3 mx-2 bg-gray-300 rounded-lg w-96 min-h-[8rem] flex flex-col">
+            <div class="flex flex-row w-full justify-between border-b-2" :style="{borderColor: lane.border}">
+              <p class="text-gray-700">
+                {{ lane.name }}
+              </p>
+              <button @click="addTask(lane.id)" class="text-gray-700">Add Task</button>
+            </div>
+            <Column :list="lane" :id="lane.id" @close="closeTask"/>
           </div>
-          <Column :list="lane" :id="lane.id" @close="closeTask"/>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
